fae7cd15e6a1e5cec50a39d3a30e593bae76f878
Joshua Camacho <joshcamacho.413@gmail.com>
2023-04-28 13:55:34 +0800

    register

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,13 @@ const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, UnauthenticatedError } = require("../errors");
 
 const register = async (req, res) => {
-  const user = await User.create({ ...req.body });
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    throw new BadRequestError("Please provide name, email, and password");
+  }
+
+  const user = await User.create({ name, email, password });
   const token = user.createJWT();
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
